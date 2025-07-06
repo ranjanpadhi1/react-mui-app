3c972b555e62e5031db74b90fe0ccba4f318d258
ranjanpadhi1 <tr.padhi91@gmail.com>
2025-07-06 23:36:07 +0530

    ok

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,5 @@
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
 import { Card } from '@mui/material';
 
 export default function Home() {
